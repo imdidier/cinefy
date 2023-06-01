e860a68143bf83b9363785dac169fe3c7bc57e5b
Didier Junco <imdidierjunco@gmail.com>
2023-05-31 22:41:20 -0500

    Realizando las implementaciones para agregar los favotritos

--- lib/infrastructure/datasources/isar_datasource_impl.dart
+++ lib/infrastructure/datasources/isar_datasource_impl.dart
@@ -1,22 +1,48 @@
 import 'package:cinefy/domain/datasources/local_storage_datasource.dart';
 import 'package:cinefy/domain/entities/movie_entity.dart';
+import 'package:isar/isar.dart';
 
 class IsarDatasourceImpl extends LocalStorageDatasource {
-  @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  late Future<Isar> dataBase;
+
+  IsarDatasourceImpl() {
+    dataBase = openDataBase();
+  }
+
+  Future<Isar> openDataBase() async {
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema], directory: '', inspector: true);
+    }
+    return Future.value(Isar.getInstance());
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await dataBase;
+    final Movie? isFavoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    return isFavoriteMovie != null;
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await dataBase;
+    return isar.movies.where().offset(offset).limit(limit).findAll();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await dataBase;
+
+    final Movie? favoriteMovie =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+    if (favoriteMovie != null) {
+      //Eliminar datos de Isar
+      isar.writeTxnSync(() => isar.movies.delete(favoriteMovie.isarId!));
+      return;
+    }
+    //Insertar datos en la base de datos Isar
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 }
